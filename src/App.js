ca3d39d81d8fe91b311417de603304062650bba4
AAlkandari <96235631+AAlkandari@users.noreply.github.com>
2022-01-24 14:12:28 +0300

    JamiyaFormAndStore

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import { Route, Switch } from "react-router";
 import NavBar from "./components/NavBar";
 
 function App() {
